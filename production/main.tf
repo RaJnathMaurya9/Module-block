32430eb5a0dddca848655f93b6d5cc14400aeca6
Ravish maurya <rajnath.maurya9@outlook.com>
2025-07-06 17:21:02 +0530

    rg todu add

--- production/main.tf
+++ production/main.tf
@@ -2,3 +2,7 @@ resource azurerm_resource_group "rgre1" {
   name     = var.rg_name
   location = var.location
 }
+resource azurerm_resource_group "rgre2" {
+  name     = var.rg_name1
+  location = var.location
+}
